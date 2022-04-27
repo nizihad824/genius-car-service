f260738577a531fec7e192204512b79944fe447c
nizihad824 <mdnurulislam97@gmail.com>
2022-04-27 13:19:15 +0200

    displayed images

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
              <>
             
             <Services></Services>
+            <Experts></Experts>
             
         </>
         </div>
