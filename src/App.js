85bc2d96ee15fb939a4967a57f4c681b7653243b
nizihad824 <mdnurulislam97@gmail.com>
2022-04-27 03:27:17 +0200

    set routes forr component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div>
-      
-    </div>
+    <Header></Header>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+    </Routes>
+    <Footer></Footer>
+  </div>
     
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <p>copyright @</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
