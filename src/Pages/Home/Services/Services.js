4fbe2c333ecf11467392c63f7bf238517fdff1d9
nizihad824 <mdnurulislam97@gmail.com>
2022-04-27 03:44:51 +0200

    add json file for loading data

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect( ()=>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data));
+    }, [])
+
     return (
         <div>
             <h2>Services:</h2>
