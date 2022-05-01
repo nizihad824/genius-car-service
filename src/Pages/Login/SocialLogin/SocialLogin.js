c112699eee0540540d8fa65d154d9b1bc73b93ff
nizihad824 <mdnurulislam97@gmail.com>
2022-05-01 18:32:23 +0200

    sign in with Github

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,21 +2,21 @@ import React from 'react';
 import google from '../../../images/social/google.png';
 import facebook from '../../../images/social/facebook.png';
 import github from '../../../images/social/github.png';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
     const navigate =useNavigate();
     let errorElement;
-    if(error){
-        errorElement = <div>
-            <p>Error :{error.message}</p>
-        </div>
+
+    if (error || error1) {
+        errorElement = <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
     }
 
-    if(user){
+    if(user||user1){
         navigate('/home')
     }
    
@@ -46,7 +46,7 @@ const SocialLogin = () => {
                     <span className='px-2'>Facebook Sign In</span>
                 </button>
                 <button
-                    // onClick={() => signInWithGithub()}
+                    onClick={() => signInWithGithub()}
                     className='btn btn-info w-50 d-block mx-auto'>
                     <img style={{ width: '30px' }} src={github} alt="" />
                     <span className='px-2'>Github Sign In</span>
