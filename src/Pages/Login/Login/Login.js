33df21dde1b8687ca3d6fdebb7105be46560e01b
nizihad824 <mdnurulislam97@gmail.com>
2022-05-01 17:09:22 +0200

    social media icon added and new component

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -42,15 +42,11 @@ const Login = () => {
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
+                 
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import google from '../../../images/social/google.png';
+import facebook from '../../../images/social/facebook.png';
+import github from '../../../images/social/github.png';
+
+const SocialLogin = () => {
+    return (
+        <div>
+             <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className='bg-primary w-50'>
+
+                </div>
+                </div>
+            
+        </div>
+    );
+};
+
+export default SocialLogin;
