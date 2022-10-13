03784cab9994aacf682adac3e7ab18a4752f9c71
nizihad824 <mdnurulislam97@gmail.com>
2022-10-14 01:05:24 +0200

    get orders api

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import axiosPrivate from 'axios';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Orders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        
+        const getOrders = async() =>{
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+          
+                const {data} = await axiosPrivate.get(url);
+                setOrders(data);
+          
+           
+        }
+        getOrders();
+    },[user])
     return (
         <div>
+             <h2>Your orders: {orders.length}</h2>
             
         </div>
     );
