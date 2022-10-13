1abc9f7090eb7320f3219043de152ca7eba179f6
nizihad824 <mdnurulislam97@gmail.com>
2022-10-13 16:40:56 +0200

    made useService details hook for checkout and servicedetails component

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -2,23 +2,18 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import useServiceDetails from '../../hooks/useServiceDetails';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
+    const [service]=useServiceDetails(serviceId)
 
-    const [service,setService]=useState({})
-
-    useEffect(()=>{
-        const url =`http://localhost:5000/service/${serviceId}`
-        fetch(url)
-        .then(res =>res.json())
-        .then(data =>setService(data))
-    },[])
+   
     return (
         <div>
             <h2>You are about to book: {service.name}</h2>
             <div className='text-center'>
-                <Link to="/checkout">
+                <Link to={`/checkout/${serviceId}`}>
                     <button className='btn btn-primary'>Proceed Checkout</button>
                 </Link>
             </div>
